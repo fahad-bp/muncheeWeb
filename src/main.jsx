38e0da37615d828872e91b9cf66c5759614ec86b
fahad-bp <fahad@bitspro.com>
2024-05-24 01:19:56 +0500

    Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { chakraProvder } from "@chakra-ui/react";
-
+import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <chakraProvder>
+    <ChakraProvider>
       <App />
-    </chakraProvder>
+    </ChakraProvider>
   </React.StrictMode>
 );
